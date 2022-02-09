a03680bda08c8730165cf57e9963a7554ea2f8f7
sparrow001 <66333207+sparrow001@users.noreply.github.com>
2022-02-08 21:12:47 -0500

    increase security and add release chart

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,9 +1,8 @@
-import { readUserData, getCookie } from './functions.js';
+import { readUserData, getCookie, setcookiehour, deleteCookie } from './functions.js';
 
 
 var user = getCookie('user');
 var data = readUserData(user);
-console.log(data.display_name)
 
 var fill = ["display_name", "email", "first_name", "last_name", "token"];
 
@@ -33,17 +32,50 @@ data.then(function(data) {
 });
 
 document.getElementById("submit").addEventListener("click", submit);
+window.onload = sessionid()
+function sessionid() {
+    let text = document.getElementById("session")
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~-_!@#$%^&*()_+{}|:"<>?[]\;\',./';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < 5; i++ ) {
+        var result;
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    text.innerHTML = "Session ID: " + result.split("fined")[1];
+    setcookiehour("session", btoa(result.split("fined")[1]))
+}
+let errors;
 function submit() {
-    let dname = btoa(document.getElementById("dname").value.toLowerCase());
-    let email = btoa(document.getElementById("em").value.toLowerCase());
-    let fname = btoa(document.getElementById("fname").value.toLowerCase());
-    let lname = btoa(document.getElementById("lname").value.toLowerCase());
-    let token = btoa(document.getElementById("tok").value.toLowerCase());
-    if (atob(dname) == "s" && atob(email) == "c" && atob(fname) == "h" && atob(lname) == "o" && atob(token) == "l") {
+    let dname = btoa(document.getElementById("dname").value);
+    let email = btoa(document.getElementById("em").value);
+    let fname = btoa(document.getElementById("fname").value);
+    let lname = btoa(document.getElementById("lname").value);
+    let token = btoa(document.getElementById("tok").value);
+    let sessionkey = atob(getCookie("session")).split("");
+    console.log(sessionkey[0])
+    if (atob(dname) == sessionkey[0] && atob(email) == sessionkey[1] && atob(fname) == sessionkey[2] && atob(lname) == sessionkey[3] && atob(token) == sessionkey[4]) {
         window.onbeforeunload = null
         window.location.replace("../secure/dash.html");
     }else {
-
+        sessionid()
+        errors++
+        if (errors == 3) {
+            deleteCookie("user")
+            const firebaseConfig = {
+                apiKey: "AIzaSyBff6gLXbUMW0rnq4186O9d9896toadZ30",
+                authDomain: "school-site-b799d.firebaseapp.com",
+                projectId: "school-site-b799d",
+                storageBucket: "school-site-b799d.appspot.com",
+                messagingSenderId: "704109356346",
+                appId: "1:704109356346:web:be6a9266093b06dbd81c03",
+                measurementId: "G-VXQWNZJ3MX"
+            };
+            const app = initializeApp(firebaseConfig);
+            const auth = getAuth(app);
+            signOut(auth)
+        }else {
+            
+        }
     }
 }
 
