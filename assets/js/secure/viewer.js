ae1fd269054bab21da293bf0ff38e3d0f520173c
sparrow001 <66333207+sparrow001@users.noreply.github.com>
2022-02-16 20:53:20 -0500

    customize video

--- assets/js/secure/viewer.js
+++ assets/js/secure/viewer.js
@@ -1,5 +1,17 @@
 import { getCookie, deleteCookie } from '../functions.js'
 
+document.getElementById('hide').addEventListener('click', hidehandler);
+document.getElementById('show').addEventListener('click', showhandler);
+
+function hidehandler() {
+    var viewer = document.getElementById('medvid');
+    viewer.style.visibility = 'hidden';
+}
+
+function showhandler() {
+    var viewer = document.getElementById('medvid');
+    viewer.style.visibility = 'visible';
+}
 var medid = getCookie("medid")
 var url = "https://redir.sparrow001.workers.dev/" + atob(medid)
 let response = await fetch(url, {
@@ -14,3 +26,4 @@ source.setAttribute('src', final);
 source.setAttribute('type', 'video/mp4');
 
 player.appendChild(source);
+
